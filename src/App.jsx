18258316f565983995be7439994572f3e00d795f
codeRastogi <harshit1122ras@gmail.com>
2024-08-04 20:41:59 +0530

    Home & nav bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import "./app.scss";
+import Hero from "./component/hero/hero";
+import NavBar from "./component/navbar/NavBar";
+import Parallax from "./component/parallax/parallax";
+
 const App = () => {
   return <div>
-    <section>Hero</section>
-    <section>parallax</section>
+    <section id="Homepage">
+      <NavBar/>
+      <Hero/>
+    </section>
+    <section id="Services">
+      <Parallax type = "services"/>
+    </section>
     <section>services</section>
-    <section>parallax</section>
-    
+    <section id="Portfolio"><Parallax type= "portfolio"/></section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section id="Contact">contact</section>
   </div>;
 };
 
